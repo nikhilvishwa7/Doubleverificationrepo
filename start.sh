e77e4fa5b2712be38dcec2d03ca8ad645bc2bbc0
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2023-05-07 21:36:53 +0530

    Change Start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kdbotz/UrlShortner-AutoFilter-Bot.git /UrlShortner-AutoFilter-Bot
+  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /UrlShortner-AutoFilter-Bot
+  git clone $UPSTREAM_REPO /DQTheFileDonorBot
 fi
-cd /UrlShortner-AutoFilter-Bot
+cd /DQTheFileDonorBot
 pip3 install -U -r requirements.txt
-echo "Starting Lucifer...."
+echo "Starting DQ-The-File-Donor...."
 python3 bot.py
