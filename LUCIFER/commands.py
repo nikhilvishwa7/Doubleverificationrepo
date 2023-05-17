eb1ebfdac0ce7877568c06bd92036b310e946000
nikhilvishwa7 <123230101+nikhilvishwa7@users.noreply.github.com>
2023-05-17 20:49:14 +0530

    Update commands.py

--- LUCIFER/commands.py
+++ LUCIFER/commands.py
@@ -24,8 +24,8 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                   ],[
-                    InlineKeyboardButton('Uᴘᴅᴀᴛᴇs', url='https://t.me/CornersOfficial'),
-                    InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ', url='https://t.me/Request_Corner1')
+                    InlineKeyboardButton('Uᴘᴅᴀᴛᴇs', url='https://t.me/Update_Channel7'),
+                    InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ', url='https://t.me/Update_Channel7')
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         kd = await message.reply_photo(
